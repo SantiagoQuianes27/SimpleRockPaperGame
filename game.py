d149c64c231094a323055f08c76ecb0a61cc4a69
SantiagoQuianes27 <santiagoquianes09@gmail.com>
2024-10-09 13:33:31 +0000

    game

---
+++ game.py
@@ -0,0 +1,69 @@
+print("""Welcome to Rock, Paper, Sicssor
+--------------------------------
+  In the game you will type either R, P, or S 
+  for rock, paper, and scissor, and each of the 
+  player's score will be tracked and displayed upon Exit""")
+print()
+p1Counter = 0
+p2Counter = 0
+keepGoing = True
+while keepGoing:
+    p1 = input("player 1 > ")
+    print("""...
+    .
+    ..
+    ...
+    ..
+    .
+    ..
+    ...
+    ..
+    .
+    ..
+    ...
+    ..
+    .
+    ..
+    ...
+    ..
+    .
+    ..
+    ...
+    ..
+    .
+    no peeking!
+    """)
+    p2 = input("player 2 > ")
+    print()
+    if p1 == "R" and p2 == "S":
+        print("Player 1 takes this round!")
+        p1Counter += 1
+    elif p1 == "P" and p2 == "R":
+        print("Player 1 takes this round!")
+        p1Counter += 1
+    elif p1 == "S" and p2 == "P":
+        print("Player 1 takes this round!")
+        p1Counter += 1
+    elif p2 == "R" and p1 == "S":
+        print("Player 2 takes this round!")
+        p2Counter += 1
+    elif p2 == "P" and p1 == "R":
+        print("Player 2 takes this round!")
+        p2Counter += 1
+    elif p2 == "S" and p1 == "P":
+        print("Player 2 takes this round!")
+        p2Counter += 1
+    else:
+        print("It was a draw, go again!")
+    print()
+    exit = input("Exit? > ")
+    if exit == "yes" or exit == "Yes":
+        keepGoing = False
+    else:
+        print()
+if p1Counter > p2Counter:
+    print("Player 1 wins with", p1Counter, "wins under their belt!")
+elif p1Counter == p2Counter:
+    print("Hm, it seems like you both tied with", p1Counter, "wins, well I guess you both are winners!")
+else:
+    print("Player 2 wins with", p2Counter, "wins under their belt")
