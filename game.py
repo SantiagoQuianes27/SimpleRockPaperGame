fab10d782c04120abfabb67c0142ce511ea1d8ba
SantiagoQuianes27 <santiagoquianes09@gmail.com>
2024-10-10 12:39:09 +0000

    game

--- game.py
+++ game.py
@@ -1,8 +1,14 @@
 print("""Welcome to Rock, Paper, Sicssor
 --------------------------------
-  In the game you will type either R, P, or S 
-  for rock, paper, and scissor, and each of the 
-  player's score will be tracked and displayed upon Exit""")
+  In the game you will type either 
+  R, for rock 
+  P, for paper 
+  Sc, for scissor 
+  Sp, for spock
+  L, for lizard 
+  Yes the capitilization is required,
+  and each of the player's score will 
+  be tracked and displayed upon Exit""")
 print()
 p1Counter = 0
 p2Counter = 0
@@ -35,22 +41,34 @@ while keepGoing:
     """)
     p2 = input("player 2 > ")
     print()
-    if p1 == "R" and p2 == "S":
+    if p1 == "R" and p2 == "S" or p2 == "L":
         print("Player 1 takes this round!")
         p1Counter += 1
-    elif p1 == "P" and p2 == "R":
+    elif p1 == "P" and p2 == "R" or p2 == "Sp":
         print("Player 1 takes this round!")
         p1Counter += 1
-    elif p1 == "S" and p2 == "P":
+    elif p1 == "Sc" and p2 == "P" or p2 == "L":
         print("Player 1 takes this round!")
         p1Counter += 1
-    elif p2 == "R" and p1 == "S":
+    elif p1 == "Sp" and p2 == "R" or p2 == "Sc":
+        print("Player 1 takes this round!")
+        p1Counter += 1
+    elif p1 == "L" and p2 == "P" or p2 == "Sp":
+        print("Player 1 takes this round!")
+        p1Counter += 1
+    elif p2 == "R" and p1 == "S" or p1 == "L":
         print("Player 2 takes this round!")
         p2Counter += 1
-    elif p2 == "P" and p1 == "R":
+    elif p2 == "P" and p1 == "R" or p1 == "Sp":
         print("Player 2 takes this round!")
         p2Counter += 1
-    elif p2 == "S" and p1 == "P":
+    elif p2 == "Sc" and p1 == "P" or p1 == "L":
+        print("Player 2 takes this round!")
+        p2Counter += 1
+    elif p2 == "Sp" and p1 == "R" or p1 == "Sc":
+        print("Player 2 takes this round!")
+        p2Counter += 1
+    elif p2 == "L" and p1 == "P" or p1 == "Sp":
         print("Player 2 takes this round!")
         p2Counter += 1
     else:
